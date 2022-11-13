0110b7af7f3828b98808ad320eb926102b280e46
iputuagussetiawan <agussetiawaniputu@gmail.com>
2022-11-13 11:35:27 +0800

    routes file

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,7 @@ const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.get('/api/goals',(req,res)=>{
-  res.status(200).json({
-    message:'Get Goald',
-
-  })
-})
+app.use('/api/goals',require('./routes/goalRoutes'))
 
 app.listen(port,()=>console.log(`Server Stared on port ${port}`))
 
